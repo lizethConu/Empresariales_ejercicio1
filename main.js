a85b228e146bcbc3351bcefa1de25ca6831c2afd
Karen Covarrubias <lizeth.conu@gmail.com>
2019-02-26 22:03:39 -0700

    ejercicio 1 unidad 2

---
+++ main.js
@@ -0,0 +1,6 @@
+var sq = require("./square");
+var area = sq.ar(2);
+var perimeter=sq.per(2);
+
+console.log("El perimetro es: "+perimeter);
+console.log("El area es: "+area);
---
+++ square.js
@@ -0,0 +1,12 @@
+function area (width){
+    return width*width;
+}
+
+function perimeter (width){
+    return width*4;
+}
+
+//publicamos función
+module.exports.ar =area;
+module.exports.per=perimeter;
+
